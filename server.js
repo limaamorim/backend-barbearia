d860d3c74a60300106697526c725a177e1667e9e
nand9l <josefernand9lima@gmail.com>
2026-03-01 23:27:19 -0300

    alterando porta

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use("/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, "localhost", () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+})
